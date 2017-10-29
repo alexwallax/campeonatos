bea87c842890c5aa49973443a2edcb236870fdcb
alexwallax <31941706+alexwallax@users.noreply.github.com>
2017-10-28 22:17:56 -0200

    model

--- src/main/java/br/campeonatosgamer/model/Cadastro.java
+++ src/main/java/br/campeonatosgamer/model/Cadastro.java
@@ -3,4 +3,97 @@ package br.campeonatosgamer.model;
 
 public class Cadastro {
     
+    private int id;
+    private String nome;
+    private String senha;
+    private String email;
+    private String login;
+
+    public Cadastro() {
+    }
+
+    public Cadastro(int id) {
+        this.id = id;
+    }
+
+    public Cadastro(int id, String nome, String senha, String email, String login) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cadastro other = (Cadastro) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cadastro{" + "id=" + id + ", nome=" + nome + ", senha=" + senha + ", email=" + email + ", login=" + login + '}';
+    }
+    
+    
+    
 }
--- src/main/java/br/campeonatosgamer/model/Usuario.java
+++ src/main/java/br/campeonatosgamer/model/Usuario.java
@@ -3,4 +3,11 @@ package br.campeonatosgamer.model;
 
 public class Usuario {
     
+    int id;
+    String nome;
+    String senha;
+    Login login;
+    
+    
+    
 }
